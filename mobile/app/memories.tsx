1ea8e521de0106919f5bda8383c9fc4f8a56b819
gi-giardini <giovana.borges@accurate.com.br>
2023-05-23 17:17:26 -0300

    load memories

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,89 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect } from 'react'
+
+import Icon from '@expo/vector-icons/Feather'
+import NLWLogo from '../src/assets/logo.svg'
+import { api } from '../src/lib/api'
 
 export default function Memories() {
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    return await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between px-8">
+        <NLWLogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50" />
+            <Text className="font-body text-xs text-gray-100">12 de abril</Text>
+          </View>
+          <View className="space-y-4 px-8">
+            <Image
+              source={{
+                uri: 'http://192.168.0.8:3333/uploads/d9779090-cf51-4d2a-aed4-061941be8ee6.jpg',
+              }}
+              className="aspect-video w-full rounded-lg"
+              alt=""
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Texto teste
+            </Text>
+            <Link href="/memories/id" asChild>
+              <TouchableOpacity className="flex-row items-center gap-2">
+                <Text className="font-body text-sm text-gray-200">
+                  Leia mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
